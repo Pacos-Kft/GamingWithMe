1576b978adc0cdbd0aa904a131641cea7563c0f1
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-18 15:13:54 +0200

    Create EsportPlayer.cs

---
+++ backend/GamingWithMe/GamingWithMe.Domain/Entities/EsportPlayer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Domain.Entities
+{
+    public class EsportPlayer
+    {
+        public Guid Id { get; set; }
+
+        public string UserId { get; set; }
+        public virtual IdentityUser User { get; set; }
+
+        public string Avatar { get; set; }
+        public string Bio {  get; set; }
+        public ICollection<string> Languages { get; set; }
+        //TODO Review
+        public int Earnings { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public EsportPlayer()
+        {
+            
+        }
+
+        public EsportPlayer(string userId) 
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Avatar = string.Empty;
+            Bio = string.Empty;
+            Languages = new List<string>();
+            Earnings = 0;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
