dda8f9ca3e331108c5b1360d9da385c83b3a89ac
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-18 15:20:09 +0200

    Create EsportPlayerDto.cs

---
+++ backend/GamingWithMe/GamingWithMe.Application/Dtos/EsportPlayerDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Dtos
+{
+    public record EsportPlayerDto(
+        string username, 
+        string avatarurl, 
+        string bio, 
+        List<string> languages, 
+        int earnings, 
+        DateTime joined);
+}
