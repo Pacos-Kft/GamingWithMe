c708517b371dd317fb71187b6532470114bd8edb
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-20 13:44:51 +0200

    register dto, command, handler

---
+++ backend/GamingWithMe/GamingWithMe.Application/Dtos/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Dtos
+{
+    public enum PlayerType { Regular, Esport }
+
+    public record RegisterDto(
+        string email,
+        string password,
+        string username,
+        PlayerType PlayerType
+        );
+    
+}
