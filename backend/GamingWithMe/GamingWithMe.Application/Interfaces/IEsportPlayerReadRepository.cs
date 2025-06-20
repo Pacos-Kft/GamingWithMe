8380153578e3f0ff7e8ec9f6267b75da6dca009a
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-20 21:45:02 +0200

    get esport player profile

---
+++ backend/GamingWithMe/GamingWithMe.Application/Interfaces/IEsportPlayerReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using GamingWithMe.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Interfaces
+{
+    public interface IEsportPlayerReadRepository
+    {
+        Task<EsportPlayerDto?> GetProfileByUsernameAsync(
+            string username,
+            CancellationToken ct = default);
+    }
+}
---
+++ backend/GamingWithMe/GamingWithMe.Infrastructure/Repositories/EsportPlayerReadRepository.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GamingWithMe.Application.Dtos;
+using GamingWithMe.Application.Interfaces;
+using GamingWithMe.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Infrastructure.Repositories
+{
+    public sealed class EsportPlayerReadRepository
+    : IEsportPlayerReadRepository
+    {
+        private readonly ApplicationDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public EsportPlayerReadRepository(ApplicationDbContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        public async Task<EsportPlayerDto?> GetProfileByUsernameAsync(
+            string username, CancellationToken ct = default)
+        {
+            return await _ctx.EsportPlayers
+                .AsNoTracking()
+                .Where(p => p.Username == username)
+                .ProjectTo<EsportPlayerDto>(_mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync();
+                
+        }
+    }
+
+}
