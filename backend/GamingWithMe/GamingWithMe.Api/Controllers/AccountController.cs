4fff5d9bbbfb60ec673aaaeb10f0b93add43f9a1
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-20 13:43:27 +0200

    Create AccountController.cs

---
+++ backend/GamingWithMe/GamingWithMe.Api/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using GamingWithMe.Application.Commands;
+using GamingWithMe.Application.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamingWithMe.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto dto, CancellationToken cancellationToken)
+        {
+            var id = await _mediator.Send(new RegisterProfileCommand(dto), cancellationToken);
+            return Ok(id);
+        }
+    }
+}
