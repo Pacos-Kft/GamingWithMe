4943b468729d2494fc20b088b36689efcb482b1e
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-20 13:44:01 +0200

    player entities

--- backend/GamingWithMe/GamingWithMe.Domain/Entities/EsportPlayer.cs
+++ backend/GamingWithMe/GamingWithMe.Domain/Entities/EsportPlayer.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,34 +6,23 @@ using System.Threading.Tasks;
 
 namespace GamingWithMe.Domain.Entities
 {
-    public class EsportPlayer
+    public class EsportPlayer : PlayerBase
     {
-        public Guid Id { get; set; }
-
-        public string UserId { get; set; }
-        public virtual IdentityUser User { get; set; }
-
-        public string AvatarUrl { get; set; }
         public string Bio {  get; set; }
         public ICollection<string> Languages { get; set; }
         //TODO Review
         public int Earnings { get; set; }
-        public DateTime CreatedAt { get; set; }
 
-        public EsportPlayer()
+        public EsportPlayer() : base()
         {
             
         }
 
-        public EsportPlayer(string userId) 
+        public EsportPlayer(string userId, string username) : base(userId, username) 
         {
-            Id = Guid.NewGuid();
-            UserId = userId;
-            AvatarUrl = string.Empty;
             Bio = string.Empty;
             Languages = new List<string>();
             Earnings = 0;
-            CreatedAt = DateTime.UtcNow;
         }
     }
 }
---
+++ backend/GamingWithMe/GamingWithMe.Domain/Entities/PlayerBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Domain.Entities
+{
+    public class PlayerBase
+    {
+        public Guid Id { get; set; }      
+
+        public string UserId { get; set; }
+        public virtual IdentityUser User { get; set; }
+
+        public string Username { get; set; }
+
+        public string AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public PlayerBase()
+        {
+
+        }
+
+        public PlayerBase(string userId, string username)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Username = username;
+            AvatarUrl = string.Empty;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
