1043def9bfc9bba3454d7873b9c1e0fa10de508d
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-22 18:16:07 +0200

    Add CreateLanguage command and handler
    Introduces CreateLanguageCommand and CreateLanguageHandler to support creating new Language entities via MediatR. The handler uses an async repository to persist the new language and returns its ID.

---
+++ backend/GamingWithMe/GamingWithMe.Application/Handlers/CreateLanguageHandler.cs
@@ -0,0 +1,30 @@
+﻿using GamingWithMe.Application.Commands;
+using GamingWithMe.Application.Interfaces;
+using GamingWithMe.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Handlers
+{
+    public class CreateLanguageHandler : IRequestHandler<CreateLanguageCommand, Guid>
+    {
+        private readonly IAsyncRepository<Language> _repo;
+
+        public CreateLanguageHandler(IAsyncRepository<Language> repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Guid> Handle(CreateLanguageCommand request, CancellationToken cancellationToken)
+        {
+            var lang = new Language { Name = request.language };
+
+            await _repo.AddAsync(lang);
+
+            return lang.Id;
+        }
+    }
+}
