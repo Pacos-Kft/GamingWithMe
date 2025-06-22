5b1f47ce4556add2815a72d60755efa99fc303ec
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-22 18:16:34 +0200

    Add command and handler to remove language from player
    Introduces DeleteLanguageFromPlayerCommand and DeleteLanguageFromPlayerHandler to support removing a language from an EsportPlayer. The handler validates the player and language existence, removes the language association if present, and updates the player entity.

---
+++ backend/GamingWithMe/GamingWithMe.Application/Handlers/DeleteLanguageFromPlayerHandler.cs
@@ -0,0 +1,51 @@
+﻿using GamingWithMe.Application.Commands;
+using GamingWithMe.Application.Interfaces;
+using GamingWithMe.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Handlers
+{
+    public class DeleteLanguageFromPlayerHandler : IRequestHandler<DeleteLanguageFromPlayerCommand, bool>
+    {
+        private readonly IAsyncRepository<EsportPlayer> _playerRepo;
+        private readonly IEsportPlayerReadRepository _esportRepo;
+        private readonly IAsyncRepository<Language> _languageRepo;
+
+        public DeleteLanguageFromPlayerHandler(
+            IAsyncRepository<EsportPlayer> playerRepo,
+            IAsyncRepository<Language> languageRepo,
+            IEsportPlayerReadRepository esportRepo)
+        {
+            _playerRepo = playerRepo;
+            _languageRepo = languageRepo;
+            _esportRepo = esportRepo;
+        }
+
+        public async Task<bool> Handle(DeleteLanguageFromPlayerCommand request, CancellationToken cancellationToken)
+        {
+            var player = await _esportRepo.GetByIdWithLanguagesAsync(request.userId, cancellationToken);
+
+            if (player is not EsportPlayer esportPlayer)
+                throw new InvalidOperationException("Player not found or not an Esport player.");
+
+            var language = (await _languageRepo.ListAsync(cancellationToken)).FirstOrDefault(x=> x.Name == request.language);
+            if (language == null)
+                throw new InvalidOperationException("Language not found.");
+
+            var entry = esportPlayer.Languages.FirstOrDefault(x => x.LanguageId == language.Id);
+
+            if (entry != null) {
+                player.Languages.Remove(entry);
+                await _playerRepo.Update(player);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
