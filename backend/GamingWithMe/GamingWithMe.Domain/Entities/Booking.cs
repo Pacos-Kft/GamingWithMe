6eb71c22ec30bdefb1d763fc3895a255aed51c7e
Ádám Bognár <bgnr.adam@gmail.com>
2025-06-24 17:24:40 +0200

    Add booking and availability entities and DTOs
    Introduced Booking and GamerAvailability entities to the domain layer for managing bookings and gamer availability. Added TimeRangeDto and WeeklyHoursDto to the application layer to represent time ranges and weekly schedules. These changes lay the groundwork for handling scheduling and availability features.

---
+++ backend/GamingWithMe/GamingWithMe.Domain/Entities/Booking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Domain.Entities
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public Guid GamerId { get; set; }
+        public Gamer Gamer { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Booking()
+        {
+            
+        }
+
+        public Booking(Guid GamerId, Guid UserId, DateTime StartTime, TimeSpan Duration)
+        {
+            this.GamerId = GamerId;
+            this.UserId = UserId;
+            this.StartTime = StartTime;
+            this.Duration = Duration;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ backend/GamingWithMe/GamingWithMe.Domain/Entities/GamerAvailability.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Domain.Entities
+{
+    public class GamerAvailability
+    {
+        public Guid Id { get; set; }
+        public Guid GamerId { get; set; }
+        public Gamer Gamer { get; set; }
+
+        public TimeSpan StartTime { get; set; } 
+        public TimeSpan EndTime { get; set; }   
+        public DayOfWeek DayOfWeek { get; set; }
+
+    }
+
+}
