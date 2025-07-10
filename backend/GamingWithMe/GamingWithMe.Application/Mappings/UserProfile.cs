70e17e599a103a128b7867682d80bc020f381e3b
Ádám Bognár <bgnr.adam@gmail.com>
2025-07-10 21:34:20 +0200

    Update UserProfile.cs

--- backend/GamingWithMe/GamingWithMe.Application/Mappings/UserProfile.cs
+++ backend/GamingWithMe/GamingWithMe.Application/Mappings/UserProfile.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
 using GamingWithMe.Application.Dtos;
 using GamingWithMe.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GamingWithMe.Application.Mappings
 {
@@ -14,14 +10,38 @@ namespace GamingWithMe.Application.Mappings
         public UserProfile()
         {
             CreateMap<User, ProfileDto>()
-                .ForMember(d => d.games,
-                    opt => opt.MapFrom(src => src.Games.Select(g => g.Game.Name)))
-                .ForMember(d => d.languages,
-                    opt => opt.MapFrom(src => src.Languages.Select(l => l.Language.Name)))
-                .ForMember(d => d.avatarurl, opt => opt.MapFrom(src => src.AvatarUrl))
+                .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(d => d.username, opt => opt.MapFrom(src => src.Username))
+                .ForMember(d => d.avatarurl, opt => opt.MapFrom(src => src.AvatarUrl))
                 .ForMember(d => d.bio, opt => opt.MapFrom(src => src.Bio))
+                .ForMember(d => d.isActive, opt => opt.MapFrom(src => src.IsActive))
+                .ForMember(d => d.languages, 
+                    opt => opt.MapFrom(src => src.Languages.Select(l => l.Language.Name).ToList()))
+                .ForMember(d => d.games, 
+                    opt => opt.MapFrom(src => src.Games.Select(g => g.Game.Name).ToList()))
+                .ForMember(d => d.tags, 
+                    opt => opt.MapFrom(src => src.Tags.Select(t => t.Tag.Name).ToList()))
+                .ForMember(d => d.hasStripeAccount, 
+                    opt => opt.MapFrom(src => !string.IsNullOrEmpty(src.StripeAccount)))
+                .ForMember(d => d.bookings, 
+                    opt => opt.MapFrom(src => src.Bookings.Select(b => new BookingSummaryDto(
+                        b.Id, b.StartTime, b.Duration, 
+                        b.Customer.Username ?? "Unknown"
+                    )).ToList()))
+                .ForMember(d => d.availability, 
+                    opt => opt.MapFrom(src => src.DailyAvailability.Select(a => new AvailabilitySlotDto(
+                        a.Id, a.Date, a.StartTime.ToString(@"hh\:mm"), 
+                        a.StartTime.Add(a.Duration).ToString(@"hh\:mm"), a.IsAvailable
+                    )).ToList()))
                 .ForMember(d => d.joined, opt => opt.MapFrom(src => src.CreatedAt));
+            
+            // Additional mappings for nested DTOs if needed
+            CreateMap<Booking, BookingSummaryDto>()
+                .ForMember(d => d.CustomerName, opt => opt.MapFrom(src => src.Customer.Username));
+            
+            CreateMap<UserAvailability, AvailabilitySlotDto>()
+                .ForMember(d => d.StartTime, opt => opt.MapFrom(src => src.StartTime.ToString(@"hh\:mm")))
+                .ForMember(d => d.EndTime, opt => opt.MapFrom(src => src.StartTime.Add(src.Duration).ToString(@"hh\:mm")));
         }
     }
 }
