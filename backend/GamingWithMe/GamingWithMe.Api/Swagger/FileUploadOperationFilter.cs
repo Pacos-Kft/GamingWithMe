d6eab30c5bbe25f4ed4a0ba30c635df5e98330e0
Ádám Bognár <bgnr.adam@gmail.com>
2025-07-16 22:00:25 +0200

    Create FileUploadOperationFilter.cs

---
+++ backend/GamingWithMe/GamingWithMe.Api/Swagger/FileUploadOperationFilter.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace GamingWithMe.Api.Swagger
+{
+    public class FileUploadOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var formParameters = context.ApiDescription.ParameterDescriptions
+                .Where(p => p.Source.Id == "Form" || p.Source.Id == "FormFile");
+
+            if (!formParameters.Any())
+            {
+                return;
+            }
+
+            var uploadFileMediaType = new OpenApiMediaType()
+            {
+                Schema = new OpenApiSchema()
+                {
+                    Type = "object",
+                    Properties = formParameters.ToDictionary(
+                        p => p.Name,
+                        p => new OpenApiSchema()
+                        {
+                            Type = p.Type == typeof(IFormFile) ? "string" : null,
+                            Format = p.Type == typeof(IFormFile) ? "binary" : null,
+                            // Handle other form fields if necessary
+                            Description = p.ModelMetadata?.Description
+                        })
+                }
+            };
+
+            operation.RequestBody = new OpenApiRequestBody
+            {
+                Content = { ["multipart/form-data"] = uploadFileMediaType }
+            };
+        }
+    }
+}
