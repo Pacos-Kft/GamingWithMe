ee0e5f088bb0321379770289fd6d44ef2c96f23e
Ádám Bognár <bgnr.adam@gmail.com>
2025-07-19 16:16:54 +0200

    Create RegisterGoogleDto.cs

---
+++ backend/GamingWithMe/GamingWithMe.Application/Dtos/RegisterGoogleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Dtos
+{
+    public record RegisterGoogleDto(
+        [Required][EmailAddress] string email,
+        [MinLength(6)] string password,
+        [Required] string username,
+        string googleId
+        );
+}
