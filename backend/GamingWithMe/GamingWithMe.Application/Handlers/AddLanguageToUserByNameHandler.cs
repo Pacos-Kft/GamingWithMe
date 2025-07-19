1bf8a2cbbcb25966b2c8e6570ffe0b7cd77e0af7
Ádám Bognár <bgnr.adam@gmail.com>
2025-07-19 20:26:48 +0200

    Add command and handler to add language to user by name
    Introduces AddLanguageToUserByNameCommand and its handler, enabling the addition of a language to a user by language name. The handler checks for user and language existence, prevents duplicates, and updates the user accordingly.

---
+++ backend/GamingWithMe/GamingWithMe.Application/Handlers/AddLanguageToUserByNameHandler.cs
@@ -0,0 +1,53 @@
+using GamingWithMe.Application.Commands;
+using GamingWithMe.Application.Interfaces;
+using GamingWithMe.Domain.Entities;
+using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Handlers
+{
+    public class AddLanguageToUserByNameHandler : IRequestHandler<AddLanguageToUserByNameCommand, bool>
+    {
+        private readonly IAsyncRepository<User> _userRepo;
+        private readonly IAsyncRepository<Language> _languageRepo;
+
+        public AddLanguageToUserByNameHandler(
+            IAsyncRepository<User> userRepo,
+            IAsyncRepository<Language> languageRepo)
+        {
+            _userRepo = userRepo;
+            _languageRepo = languageRepo;
+        }
+
+        public async Task<bool> Handle(AddLanguageToUserByNameCommand request, CancellationToken cancellationToken)
+        {
+            var user = (await _userRepo.ListAsync(cancellationToken)).FirstOrDefault(x => x.UserId == request.UserId);
+
+            if (user is null)
+                throw new InvalidOperationException("User not found");
+
+            var language = (await _languageRepo.ListAsync(cancellationToken)).FirstOrDefault(x => x.Name.ToLower() == request.LanguageName.ToLower());
+            if (language == null)
+                throw new InvalidOperationException("Language not found.");
+
+            var alreadyHasLanguage = user.Languages.Any(x => x.LanguageId == language.Id);
+
+            if (!alreadyHasLanguage)
+            {
+                user.Languages.Add(new UserLanguage
+                {
+                    LanguageId = language.Id,
+                    PlayerId = user.Id
+                });
+                
+                await _userRepo.Update(user);
+                return true;
+            }
+
+            return false; // Language already exists for user
+        }
+    }
+}
