e8e65b76f4e85b0786d5a4adf8557926e73178c6
Ádám Bognár <bgnr.adam@gmail.com>
2025-07-19 20:26:37 +0200

    Add command and handler to add user tag by name
    Introduced AddUserTagByNameCommand and its handler to allow adding a tag to a user by tag name. The handler checks for user and tag existence, prevents duplicate tags, and updates the user accordingly.

---
+++ backend/GamingWithMe/GamingWithMe.Application/Handlers/AddUserTagByNameHandler.cs
@@ -0,0 +1,53 @@
+using GamingWithMe.Application.Commands;
+using GamingWithMe.Application.Interfaces;
+using GamingWithMe.Domain.Entities;
+using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GamingWithMe.Application.Handlers
+{
+    public class AddUserTagByNameHandler : IRequestHandler<AddUserTagByNameCommand, bool>
+    {
+        private readonly IAsyncRepository<User> _userRepo;
+        private readonly IAsyncRepository<Tag> _tagRepo;
+
+        public AddUserTagByNameHandler(
+            IAsyncRepository<User> userRepo,
+            IAsyncRepository<Tag> tagRepo)
+        {
+            _userRepo = userRepo;
+            _tagRepo = tagRepo;
+        }
+
+        public async Task<bool> Handle(AddUserTagByNameCommand request, CancellationToken cancellationToken)
+        {
+            var user = (await _userRepo.ListAsync(cancellationToken)).FirstOrDefault(x => x.UserId == request.UserId);
+
+            if (user is null)
+                throw new InvalidOperationException("User not found");
+
+            var tag = (await _tagRepo.ListAsync(cancellationToken)).FirstOrDefault(x => x.Name.ToLower() == request.TagName.ToLower());
+            if (tag == null)
+                throw new InvalidOperationException("Tag not found.");
+
+            var alreadyHasTag = user.Tags.Any(x => x.TagId == tag.Id);
+
+            if (!alreadyHasTag)
+            {
+                user.Tags.Add(new UserTag
+                {
+                    TagId = tag.Id,
+                    UserId = user.Id
+                });
+                
+                await _userRepo.Update(user);
+                return true;
+            }
+
+            return false; // Tag already exists for user
+        }
+    }
+}
